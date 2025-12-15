289eafb9635deaced9cc012736cdab496570940a
youssefhebish27 <youssefhebish27@gmail.com>
2025-12-15 10:42:21 +0200

    Secured API keys

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'screens/login_screen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // تشغيل Hive (للتخزين المؤقت)
+
+  // (للتخزين المؤقت Hive تشغيل)
   await Hive.initFlutter();
   await Hive.openBox('expense_database');
 
-  // تشغيل Firebase (انسخ البيانات من صورتك وضعها هنا)
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-    apiKey: "AIzaSyARseedabLgUU9VsBri09Kol1qP5eT2XXs",
-    authDomain: "my-wallet-app-2c2bc.firebaseapp.com",
-    projectId: "my-wallet-app-2c2bc",
-    storageBucket: "my-wallet-app-2c2bc.firebasestorage.app",
-    messagingSenderId: "1035533510982",
-    appId: "1:1035533510982:web:1ac7d3e58eb7e53f5c1d47",
-    measurementId: "G-S2FWYV3YYK"
-    ),
-  );
+  // (نسخ البيانات من صورتك ووضعها هنا) -> تم استبدالها بالطريقة الآمنة 👇
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ExpenseApp());
 }
@@ -37,8 +28,6 @@ class ExpenseApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: const Color(0xFF121212),
-        useMaterial3: true,
-        fontFamily: 'Roboto',
       ),
       home: const LoginScreen(),
     );
